d3c9b4be381a9fc5fefc29f9603277201f3bffc4
sugohgoh <sugoh@Trojan-horse-29310.local>
2020-09-14 23:52:46 +0800

    fixed randomCategoryShortName

--- js/script.js
+++ js/script.js
@@ -91,7 +91,7 @@ $ajaxUtils.sendGetRequest(
 // Builds HTML for the home page based on categories array
 // returned from the server.
 function buildAndShowHomeHTML (categories) {
-  console.log(categories)
+  console.log("Categories:" + categories)
   // Load home snippet page
   $ajaxUtils.sendGetRequest(
     homeHtmlUrl,
@@ -115,7 +115,8 @@ function buildAndShowHomeHTML (categories) {
       //
       // var homeHtmlToInsertIntoMainPage = ....
       let updatedChosenCategoryName = "{{" + chosenCategoryShortName + "}}";
-      let homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, randomCategoryShortName, updatedChosenCategoryName)
+      console.log(updatedChosenCategoryName)
+      let homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", updatedChosenCategoryName)
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
